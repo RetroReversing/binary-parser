3150f2a3750f3090b1048256bca3ee061fd27f3a
Alasdair Morrison <alasdair_morrison@apple.com>
2018-11-30 14:07:39 +0000

    Moved all packages into one browserify component

---
+++ main.js
@@ -0,0 +1,4 @@
+import Parser from "./lib/binary_parser";
+import { FileUnparser, ReactUnparser } from './lib/binary_unparser';
+const Buffer = require('buffer/').Buffer;
+export { Parser, FileUnparser, ReactUnparser, Buffer }
